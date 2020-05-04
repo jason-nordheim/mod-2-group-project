7fbb4f03f09a175024dec173feada6bf55b27366
github username <github email address>
2020-05-04 15:15:58 -0600

    author has create, show,update and index. fetch added to index.js for jokes

--- jokes-api/app/controllers/authors_controller.rb
+++ jokes-api/app/controllers/authors_controller.rb
@@ -1,12 +1,38 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.all
+    render json: @authors
   end 
+
   def show 
+    if params[:id]
+      @author = Author.find(params[:id])
+      render json: @author
+    else
+      index()
+    end
   end 
+
   def create 
+    @author = Author.new(name: params[:name])
+    if @author.save
+      render json: @author
+    else 
+      render text: "Save failed"
+    end
   end 
+
   def update 
+    if params[:id]
+      @rating.name = params[:name] 
+      if @author.save
+        render json: @author
+      else
+        render text: "Failed to update Author"
+      end
+    else
+      render text: "ID required to update Author"
+    end
   end 
-  def destroy 
-  end 
+
 end
--- jokes-app/scripts/index.js
+++ jokes-app/scripts/index.js
@@ -1,3 +1,8 @@
 let jokeSetup = document.querySelector('#joke-setup')
 let jokePunchline = document.querySelector('#joke-punch')
 let jokeCategory = document.querySelector('#joke-category')
+
+fetch("http://localhost:3000/jokes")
+    .then(response => response.json())
+    .then(console.log)
+
