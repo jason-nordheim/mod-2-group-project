5aaf8eeccaf04b66b5cac056cd5251bc4a985f7e
jason-nordheim <jason.nordheim@gmail.com>
2020-05-05 14:32:01 -0600

    added a seed for dad jokes api author

--- jokes-api/db/seeds.rb
+++ jokes-api/db/seeds.rb
@@ -3,6 +3,9 @@ default_user = Author.create(name: 'default')
 colbert = Author.create(name: 'Steven Colbert')
 dad = Author.create(name: "Dad")
 
+dadJokesAPI = Author.create(name: 'Dad Jokes API'); 
+
+
 Joke.create(category: "general", setup: 'What did Michael Jackson name his denim store?',punchline: 'Billy Jeans', author_id: default_user.id) 
 Joke.create(category: "technology", setup: "What do you call a computer floating in the ocean?", punchline: "A Dell rolling in the deep",  author_id: default_user.id)
 Joke.create(category: "pop culture", setup: "What do you call a ride sharing app that serves breakfast?", punchline: "Eggs Uber Easy",  author_id: default_user.id)
