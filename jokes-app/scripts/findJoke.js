99ded28e4ddd8167a9591c23af007c148226e803
jason-nordheim <jason.nordheim@gmail.com>
2020-05-06 09:12:00 -0600

    get the skeleton for findJokes.html

--- jokes-app/scripts/findJoke.js
+++ jokes-app/scripts/findJoke.js
@@ -55,11 +55,45 @@ function renderAuthors() {
 }
 
 function filterJokes() {
-  console.log('i am filtering jokes')
+  const displayedJokes = document.getElementsByClassName('displayed-joke');
+  console.log(displayedJokes);
 }
 
 function renderJokes() {
-  console.log('i am rendering jokes')
+  console.log('jokes', JOKES)
+  const jokesContainer = document.querySelector('#jokes-container');
+  JOKES.forEach(j => {
+    console.log("j", j)
+    const container = document.createElement('div');
+    container.classList.add('displayed-joke')
+    const category = document.createElement('div')
+    category.innerHTML = `<p class='joke-category'><b>Category:<b>${j.category}</p>`;
+    container.appendChild(category);
+
+    const setup = document.createElement('div');
+    setup.innerHTML = `<p class='joke-setup'><b>Setup:</b>${j.setup}</p>`;
+    container.appendChild(setup);
+
+    const punchline = document.createElement('div');
+    punchline.innerHTML = `<p class='joke-punchline'><b>Punchline:</b>${j.punchline}</p>`
+    container.appendChild(punchline);
+
+    const ratingForm = createRatingForJoke(j);
+    container.appendChild(ratingForm);
+
+    jokesContainer.appendChild(container);
+  })
+}
+
+function createRatingForJoke(joke) {
+  const form = document.createElement('form');
+  form.action = "http://localhost:3000/ratings";
+  form.method = "POST";
+  form.innerHTML = `<label for="rating">Rate This Joke</label>
+  <input min="1" max="5" type="number" name="rating" class="rating" for="rating" " />
+  <input type="hidden" name="joke_id" value="${joke.id}"> 
+  <input type="submit" value="Click Here!">`;
+  return form;
 }
 
 function renderCategories() {
