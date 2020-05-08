21b2d2a953ee14ff3028769b94cd76a1e1fa5e59
jason-nordheim <jason.nordheim@gmail.com>
2020-05-08 10:58:09 -0600

    added form validation

--- jokes-app/scripts/newJoke.js
+++ jokes-app/scripts/newJoke.js
@@ -14,7 +14,7 @@ fetch('http://localhost:3000/authors')
 function getCategories(jokes) {
     jokes.forEach(joke => {
         if (jokeCategories.includes(joke.category)) {
-            
+
         } else {
             jokeCategories.push(joke.category)
         }
@@ -23,7 +23,7 @@ function getCategories(jokes) {
     console.log(jokeCategories)
 }
 
-function createCategoryOptions () {
+function createCategoryOptions() {
     jokeCategories.forEach(category => {
         option = document.createElement('option')
         option.name = category
@@ -35,33 +35,69 @@ function createCategoryOptions () {
 
 function createForm() {
     jokeAttributes.forEach(attribute => {
-        let label = document.createElement('label')
-        let input = document.createElement("input")
+        const label = document.createElement('label')
+        const input = document.createElement("input")
 
-        label.htmlFor = attribute 
+        label.htmlFor = attribute
         label.innerText = `${attribute}`
 
         input.attributes = "required"
         input.htmlFor = attribute
         input.name = attribute
         input.id = attribute
+        input.oninput = ValidateEntry;
 
-        jokeForm.appendChild(label)
+        jokeForm.appendChild(label);
         jokeForm.appendChild(input)
     });
-    createAuthor ();
-    createSubmit ();
+    createAuthor();
+    createSubmit();
 }
-function createSubmit () {
-    let submit = document.createElement('input')
-    submit.type = "submit"
-    submit.value = "create"
+
+function ValidateEntry(event) {
+    const elementID = event.target.id;
+    const element = document.querySelector(`#${elementID}`);
+    //console.log(`inner text: ${element.value}`)
+    if (element.value.length == 0) {
+        element.classList.remove('valid-option')
+        element.classList.remove('invalid-option')
+    } else if (element.value.length < 3) {
+        //console.log('invalid')
+        element.classList.remove('valid-option')
+        element.classList.add('invalid-option')
+    } else {
+        //console.log('valid')
+        element.classList.remove('invalid-option')
+        element.classList.add('valid-option')
+    }
+    shouldEnableSubmit();
+}
+
+function shouldEnableSubmit() {
+    const submit = document.querySelector('#submit-btn');
+    const setup = document.querySelector('#setup');
+    const punchline = document.querySelector('#punchline');
+    const author = document.querySelector('#author_id');
+
+    if (setup.value.length > 3 && punchline.value.length > 3 && author.value.length > 3) {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+}
+
+function createSubmit() {
+    const submit = document.createElement('input');
+    submit.id = 'submit-btn';
+    submit.type = "submit";
+    submit.value = "create";
+    submit.disabled = true;
     jokeForm.appendChild(submit)
 }
 
 function createAuthor() {
-    let label = document.createElement('label')
-    let input = document.createElement("input")
+    const label = document.createElement('label')
+    const input = document.createElement("input")
 
     label.htmlFor = "author_id"
     label.innerText = "Author"
@@ -70,6 +106,7 @@ function createAuthor() {
     input.htmlFor = "author_id"
     input.name = "author_id"
     input.id = "author_id"
+    input.oninput = ValidateEntry;
 
     jokeForm.appendChild(label)
     jokeForm.appendChild(input)
